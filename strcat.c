dce8b6af45c448760d4bde1da305d550555b814c
VM IUT <vm@iut>
2018-06-01 17:10:40 +0200

    Exo 1 et 2 tp 6

--- strcat.c
+++ strcat.c
@@ -1,3 +1,26 @@
 #include<stdio.h>
 
+char *my_strcat(char *dest, const char *src){
+	char *p = dest;
+	const char *l = src;
+	while(*p != '\0'){
+		p++;
+	}
+	while(*l !='\0'){
+		*p=*l;
+		p++;
+		l++;
+	}
+	*p='\0';
+	return dest;
+}
+
+int main(void){
+	char dest[] = "Bonjour ";
+	const char src[] = "tout le monde";
+	char *n;
+	n=my_strcat(dest,src);
+	printf("Texte concatené : %s",n);
+}
+
 
