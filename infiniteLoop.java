9246bd290e4c170576900bce079ed8a247350cca
VM IUT <vm@iut>
2018-06-08 11:30:01 +0200

    Debordement2

---
+++ infiniteLoop.java
@@ -0,0 +1,9 @@
+public class infiniteLoop{
+
+	public static void main(String[] args){
+		int n=1;
+		while(n==1){
+			System.out.println("BLEH");
+		}
+	}
+}
---
+++ infinite_loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+	int n=1;
+	while(n==1){
+		printf("BLEH");
+
+	}
+}
